d9523e5ec8bbc6a2948d00c34c7bd6f65047434e
AlexKor94 <sasha1994work@gmail.com>
2023-07-09 15:43:36 +0300

    improve duplication of id and style in row

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,47 +9,67 @@ import AddEmplee from '../form/addEmplee';
 import './app.css';
 
 function App() {
-  const [data, setUsers] = useState([{
-    id: 1,
-    name: "Alex K.",
-    salary: 3000,
-    increase: false
-  },
-  {
-    id: 2,
-    name: "Fedir K.",
-    salary: 2000,
-    increase: true
-  },
-  {
-    id: 3,
-    name: "Petro K.",
-    salary: 1000,
-    increase: false
-  }]);
+  const [users, setUsers] = useState({
+    data: [{
+      id: 1,
+      name: "Alex K.",
+      salary: 3000,
+      increase: false
+    },
+    {
+      id: 2,
+      name: "Fedir K.",
+      salary: 2000,
+      increase: true
+    },
+    {
+      id: 3,
+      name: "Petro K.",
+      salary: 1000,
+      increase: false
+    }],
+    maxId: 3
+  });
 
   const [term, setTerm] = useState('');
 
+  const { data, maxId } = users;
+
   const deleteItem = (id) => {
-    setUsers(data => data.filter(user => user.id !== id));
+    // setUsers(data => data.filter(user => user.id !== id));
+    const tempData = data.filter(user => user.id !== id);
+
+    setUsers({
+      data: tempData,
+      maxId: maxId + 1
+    });
+
   }
 
   const addItem = (emplee) => {
     const tempData = [...data, {
-      id: data.length + 1,
+      id: maxId + 1,
       ...emplee,
       increase: false
     }];
-    setUsers(tempData);
+    setUsers({
+      data: tempData,
+      maxId: maxId + 1
+    });
   }
 
   const onIncrease = (id, prop) => {
-    setUsers(data => data.map(user => {
+    const tempData = data.map(user => {
       if (user.id === id) {
         return { ...user, [prop]: !user[prop] }
       }
       return user;
-    }));
+    });
+
+    setUsers({
+      data: tempData,
+      maxId: maxId + 1
+    });
   }
 
   const searchEmp = (elements, term) => {
